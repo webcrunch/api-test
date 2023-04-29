a73498fb147514bc72fc351af5612e2f557dd6dd
webcrunch <jarllinduist@gmail.com>
2023-04-29 18:07:11 +0200

    removing github actions and adding acl roules

--- backend/Acl.js
+++ backend/Acl.js
@@ -6,16 +6,27 @@ module.exports = class Acl {
   // req.sesssion.user -> logged in user if any
 
   static checkRoute(req, table, method, isTable, isView) {
-    return true; // dont need acl for this project
+    
+    // working with black listing so we default is everyone forbidden to use all the routes
     let role = req.session.user ? (req.session.user.userRole || "logged in") : 'not logged in'
-    console.log(['role:' + role, 'url:' + req.url, 'table:' + table, 'method:' + method,].join('\n'));
+    // console.log([' ---------------------', 'role:' + role, 'url:' + req.url, 'table:' + table, 'method:' + method,].join('\n'));
     //'isTable:' + isTable, 'isView:' + isView,
         
+    // this route is open for not logged in  users and only for method post so they could logg in to the site
     if (role === 'not logged in' && table === 'users' && method === 'post') return true;
 
-    if (role === 'not logged in' && method !== 'get') return false;
+    // let all users get tales and views for the site. 
+    if ( table === 'table:tables_and_views' &&  method === 'get') return true;
+    
+    // all logged in/admins could approach all get endpoints
+    if (role === 'logged in' || 'Admin' && method === 'get') return true;
 
-    if (role !== 'admin' && table === 'users') return false;
+    // The roll as admin has the ability to do anything. 
+    if (role === 'Admin') return true;
+
+    // if (role === 'not logged in' && method !== 'get') return false;
+
+    // if (role !== 'admin' && table === 'users') return false;
 
     return true;
   }
